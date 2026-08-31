ffe778ffb949e1507c950501f9ad323295a4320f
bobxzy-0 <260326404+bobxzy-0@users.noreply.github.com>
2026-08-31 16:08:01 +0800

    refactor: remove Mozilla Monitor integration

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -4,4 +4,3 @@ from app.models.models import FindingSourceEnum
 def test_provider_sources_are_mappable():
     assert FindingSourceEnum("hudson_rock") is FindingSourceEnum.hudson_rock
     assert FindingSourceEnum("hibp_breach") is FindingSourceEnum.hibp_breach
-    assert FindingSourceEnum("mozilla_monitor") is FindingSourceEnum.mozilla_monitor
